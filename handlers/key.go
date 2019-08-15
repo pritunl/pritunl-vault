7a87ac7f8a1321e263cd624b403ebcd8b3153d33
Zachary Huff <zach.huff.386@gmail.com>
2019-08-15 02:45:05 -0400

    Add key handlers

---
+++ handlers/key.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-vault/utils"
+	"github.com/pritunl/pritunl-vault/vault"
+)
+
+func keyGet(c *gin.Context) {
+	payload, err := vault.Primary.GetServerKey()
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	c.JSON(200, payload)
+}
+
+func keyPost(c *gin.Context) {
+	payload := &vault.Payload{}
+
+	err := c.Bind(payload)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	err = vault.Primary.LoadHostKey(payload)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	c.Status(200)
+}
+
+func masterPost(c *gin.Context) {
+	payload := &vault.Payload{}
+
+	err := c.Bind(payload)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	err = vault.Primary.LoadMasterKey(payload)
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	c.Status(200)
+}
+
+func initGet(c *gin.Context) {
+	initKeyData, err := vault.Primary.GetInitKey()
+	if err != nil {
+		utils.AbortWithError(c, 500, err)
+		return
+	}
+
+	c.String(200, initKeyData)
+}
